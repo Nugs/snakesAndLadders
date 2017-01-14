aa627b946d295c48b19e0723886ccf7169b14d57
Andrew Walters <andrew.walters@digital.hmrc.gov.uk>
2017-01-14 23:05:52 +0000

    Adding new Feature specification (multiple players) - pending tests.

---
+++ src/test/scala/com/alwsoft/snakesandladders/MultiplePlayersSpec.scala
@@ -0,0 +1,70 @@
+package com.alwsoft.snakesandladders
+
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{BeforeAndAfterEach, FeatureSpec, GivenWhenThen, Matchers}
+
+class MultiplePlayersSpec extends FeatureSpec with GivenWhenThen with Matchers with MockitoSugar with BeforeAndAfterEach {
+
+  private val mockDice = mock[Dice]
+  private var game: SnakesAndLadders = SnakesAndLadders(mockDice)
+
+  override def beforeEach: Unit = {
+    game = SnakesAndLadders(mockDice)
+    reset(mockDice)
+  }
+
+  feature("Determining play order") {
+    info("As a player")
+    info("I want to influence the play order")
+    info("So that player 1 doesn't always go first")
+
+    scenario("1") {
+      Given("there are two players")
+      When("the game is started")
+      Then("the players must roll dice to determine their play order")
+      pending
+    }
+
+    scenario("2") {
+      Given("the players are rolling to determine play order")
+      When("Player 1 rolls higher than Player 2")
+      Then("Player 1 rolls first")
+      pending
+    }
+
+    scenario("3") {
+      Given("the players are rolling to determine play order")
+      When("Player 2 rolls higher than Player 1")
+      Then("Player 2 rolls first")
+      pending
+    }
+
+    scenario("4") {
+      Given("the players are rolling to determine play order")
+      When("Player 1 rolls the same as Player 2")
+      Then("the players must roll again")
+      pending
+    }
+  }
+
+  feature("Following play order") {
+    info("As a player")
+    info("I want to follow the play order")
+    info("So that the game is more fair")
+
+    scenario("1") {
+      Given("it is Player 1's turn")
+      When("they have moved their token")
+      Then("it is Player 2's turn")
+      pending
+    }
+
+    scenario("2") {
+      Given("it is Player 2's turn")
+      When("they have moved their token")
+      Then("it is Player 1's turn")
+      pending
+    }
+  }
+}
